bc67bc783b2b9b5e42af5f2a04b40e517257a0f9
unhackerxd <167009046+unhackerxd@users.noreply.github.com>
2024-04-14 17:21:41 -0500

    nuevo cambio xd

--- src/main/java/Prueba123.java
+++ src/main/java/Prueba123.java
@@ -10,5 +10,6 @@ public class Prueba123 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("un lol hasta la muerte");
     }
 }
